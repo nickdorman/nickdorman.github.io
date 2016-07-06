7d45ccf33dc2e7015d7822505b35ca1bd4c7bce9
nickdorman <nickdorman@mc108615.local>
2016-07-06 19:27:34 +0100

    Gallery

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,43 @@
-// Duration is the amount of time in between slides,
- // and fade is value that determines how quickly the next image will fade in
- // $.backstretch([
- //     "./../images/big1.jpg"
- //   , "./../images/big1.jpg"
- //   , "./../images/big1.jpg"
- // ], {duration: 3000, fade: 750});
+$(document).ready(function(){
 
- $(".container").backstretch([
+ $(".container-has-image").backstretch([
    "images/big1.jpg",
    "images/big2.jpg",
    "images/big3.jpg"
 ], {duration: 6000, fade: 750});
+
+// $(function() {
+//   // Initiate Fancybox
+  $('.fancybox a').fancybox();
+
+});
+
+//
+// $(document).ready(function(){
+//    //Examples of how to assign the Colorbox event to elements
+//    $(".group1").colorbox({rel:'group1'});
+//    $(".group2").colorbox({rel:'group2', transition:"fade"});
+//    $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
+//    $(".group4").colorbox({rel:'group4', slideshow:true});
+//    $(".ajax").colorbox();
+//    $(".youtube").colorbox({iframe:true, innerWidth:640, innerHeight:390});
+//    $(".vimeo").colorbox({iframe:true, innerWidth:500, innerHeight:409});
+//    $(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
+//    $(".inline").colorbox({inline:true, width:"50%"});
+//    $(".callbacks").colorbox({
+//       onOpen:function(){ alert('onOpen: colorbox is about to open'); },
+//       onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
+//       onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
+//       onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
+//       onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
+//    });
+//
+//    $('.non-retina').colorbox({rel:'group5', transition:'none'})
+//    $('.retina').colorbox({rel:'group5', transition:'none', retinaImage:true, retinaUrl:true});
+//
+//    //Example of preserving a JavaScript event for inline calls.
+//    $("#click").click(function(){
+//       $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
+//       return false;
+//    });
+// });
